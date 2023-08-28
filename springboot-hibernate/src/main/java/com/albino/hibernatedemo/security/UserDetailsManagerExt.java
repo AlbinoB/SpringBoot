12a090f64caa30231d38c162c1b303814e12f102
Albino Braganza <albinobraganza21@gmail.com>
2023-08-28 08:43:36 +0530

    Role base access for endpoints

--- springboot-hibernate/src/main/java/com/albino/hibernatedemo/security/UserDetailsManagerExt.java
+++ springboot-hibernate/src/main/java/com/albino/hibernatedemo/security/UserDetailsManagerExt.java
@@ -2,9 +2,13 @@ package com.albino.hibernatedemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class UserDetailsManagerExt {
@@ -15,7 +19,7 @@ public class UserDetailsManagerExt {
         UserDetails albino = User.builder()
                                 .username("albino")
                                 .password("{noop}albino")
-                                .roles("ADMIN")
+                                .roles("ADMIN","TEACHER")
                                 .build();
 
         UserDetails samuel = User.builder()
@@ -26,5 +30,24 @@ public class UserDetailsManagerExt {
 
         return new InMemoryUserDetailsManager(albino,samuel);
 
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity.authorizeHttpRequests(configurer->
+                    configurer
+                        .requestMatchers(HttpMethod.GET, "/api/students").hasAnyRole("STUDENT","ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/students").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/students").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/students").hasRole("ADMIN")
+                    );
+
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        httpSecurity.csrf(csrf->csrf.disable());
+
+        return httpSecurity.build();
     }
 }
